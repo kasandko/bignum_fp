b889c90582a4406cd54512d220321055062b44de
neit_nas <neit95@mail.ru>
2024-04-10 04:06:01 +0200

    Some comments.

--- include/bfp/numeric_limits.hpp
+++ include/bfp/numeric_limits.hpp
@@ -1,9 +1,16 @@
+/**************************************************************************************************
+ * @file
+ * @brief This file implements numeric limits for `fp`. It contain class `numeric_limits`
+ * which is compatible with `std::numeric_limits`.
+ * 
+ * @note If you want that this numeric limits class to be in std namespace you need
+ * define `BFP_STD_NUMERIC_LIMITS`.
+ *************************************************************************************************/
+
 #ifndef _BFP_NUMERIC_LIMITS_H_
 #define _BFP_NUMERIC_LIMITS_H_
 
 #include <bfp/fp.hpp>
-
-//#define BFP_STD_NUMERIC_LIMITS
 
 #ifdef BFP_STD_NUMERIC_LIMITS
     #define BFP_NS std
@@ -17,6 +24,13 @@
 
 namespace BFP_NS {
 
+/**
+ * @class
+ * @brief Class with numeric limits implementations.
+ * 
+ * See [C++ documentation](https://en.cppreference.com/w/cpp/types/numeric_limits)
+ * for more information.
+ */
 template <typename TBase, BFP_ACCESS LenType Fractional, typename TBaseTypeTrait>
 class numeric_limits CLASS_SPECIALIZATION
 {
@@ -24,14 +38,26 @@ public:
 
     using fp_type = BFP_ACCESS fp<TBase, Fractional, TBaseTypeTrait>;
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// # Constants.
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    //! @brief Identifies types for which std::numeric_limits is specialized.
     static constexpr bool is_specialized = true;
+
+    
     static constexpr bool is_signed = fp_type::base_type_trait::limits::is_signed;
     static constexpr bool is_integer = false;
     static constexpr bool is_exact;
     static constexpr bool has_infinity;
     static constexpr bool has_quiet_NaN;
     static constexpr bool has_signaling_NaN;
-    static constexpr std::float_denorm_style has_denorm;
+
+    //! @brief Identifies the type that support subnormal values.
+    //!
+    //! The fixed point doesn't have denormalized values, because it is always
+    //! set to `denorm_absent`.
+    static constexpr std::float_denorm_style has_denorm = std::denorm_absent;
     static constexpr bool has_denorm_loss;
     static constexpr std::float_round_style round_style;
     static constexpr std::float_round_style round_style;
